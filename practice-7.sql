5588276ebb33f846afdb0044b931f2a55c5cbb94
MatthiasFloru <matthias.floru@kellogg.northwestern.edu>
2022-04-14 14:07:56 +0000

    performed the non graded assignment

--- practice-7.sql
+++ practice-7.sql
@@ -17,3 +17,7 @@
 -- | Entire villa                        | 75       | 2021-10-12                 |
 
 
+SELECT listings.property_type, COUNT(reviews.id), MAX(date_reviewed)
+FROM listings INNER JOIN reviews ON listings.id = reviews.listing_id
+GROUP BY listings.property_type
+;
